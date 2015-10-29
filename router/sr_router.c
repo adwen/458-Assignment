f00e48ebcffd036e0d0fa15c3029b20ec3c730bd
Julian Chow <julianchow@outlook.com>
2015-10-28 21:46:56 -0400

    Fixing compile error

--- router/sr_router.c
+++ router/sr_router.c
@@ -89,19 +89,15 @@ void sr_handlepacket(struct sr_instance* sr,
           return ;
       }
 
-  // Use ethertype() from sr_utils.c to get type of frame / packet
-  uint16_t packet_type = ethertype(packet);
-  //enum sr_ethertype ethertype = header->ether_type;  // This is wrong
-
   /* Case where we receive a ARP */
-  if (packet_type == ethertype_arp)
+  if (ethertype(packet) == ethertype_arp)
   {
     /* ARP header */
     printf("Packet is a ARP");
   }
 
   /* Case where we receive a IP */
-  else if (packet_type == ethertype_ip)
+  else if (ethertype(packet) == ethertype_ip)
   {
     /* IP header */
     printf("Packet is a IP packet");
