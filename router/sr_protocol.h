12bcfcb641485c4f25acbfcffc7898fd96f24024
Julian Chow <julianchow@outlook.com>
2015-10-28 22:58:22 -0400

    Implemented IP processing checksum and removed cancer(removed the 2-indent bullshit) from sr_protcol.h

--- router/sr_protocol.h
+++ router/sr_protocol.h
@@ -80,25 +80,25 @@
 /* Structure of a ICMP header
  */
 struct sr_icmp_hdr {
-  uint8_t icmp_type;
-  uint8_t icmp_code;
-  uint16_t icmp_sum;
-  
-} __attribute__ ((packed)) ;
+        uint8_t icmp_type;
+        uint8_t icmp_code;
+        uint16_t icmp_sum;
+
+} __attribute__ ((packed));
 typedef struct sr_icmp_hdr sr_icmp_hdr_t;
 
 
 /* Structure of a type3 ICMP header
  */
 struct sr_icmp_t3_hdr {
-  uint8_t icmp_type;
-  uint8_t icmp_code;
-  uint16_t icmp_sum;
-  uint16_t unused;
-  uint16_t next_mtu;
-  uint8_t data[ICMP_DATA_SIZE];
+        uint8_t icmp_type;
+        uint8_t icmp_code;
+        uint16_t icmp_sum;
+        uint16_t unused;
+        uint16_t next_mtu;
+        uint8_t data[ICMP_DATA_SIZE];
 
-} __attribute__ ((packed)) ;
+} __attribute__ ((packed));
 typedef struct sr_icmp_t3_hdr sr_icmp_t3_hdr_t;
 
 
@@ -108,32 +108,32 @@ typedef struct sr_icmp_t3_hdr sr_icmp_t3_hdr_t;
  * Structure of an internet header, naked of options.
  */
 struct sr_ip_hdr
-  {
+{
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-    unsigned int ip_hl:4;		/* header length */
-    unsigned int ip_v:4;		/* version */
+        unsigned int ip_hl : 4; /* header length */
+        unsigned int ip_v : 4;  /* version */
 #elif __BYTE_ORDER == __BIG_ENDIAN
-    unsigned int ip_v:4;		/* version */
-    unsigned int ip_hl:4;		/* header length */
+        unsigned int ip_v : 4;  /* version */
+        unsigned int ip_hl : 4; /* header length */
 #else
-#error "Byte ordering ot specified " 
-#endif 
-    uint8_t ip_tos;			/* type of service */
-    uint16_t ip_len;			/* total length */
-    uint16_t ip_id;			/* identification */
-    uint16_t ip_off;			/* fragment offset field */
-#define	IP_RF 0x8000			/* reserved fragment flag */
-#define	IP_DF 0x4000			/* dont fragment flag */
-#define	IP_MF 0x2000			/* more fragments flag */
-#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
-    uint8_t ip_ttl;			/* time to live */
-    uint8_t ip_p;			/* protocol */
-    uint16_t ip_sum;			/* checksum */
-    uint32_t ip_src, ip_dst;	/* source and dest address */
-  } __attribute__ ((packed)) ;
+#error "Byte ordering ot specified "
+#endif
+        uint8_t ip_tos;             /* type of service */
+        uint16_t ip_len;            /* total length */
+        uint16_t ip_id;             /* identification */
+        uint16_t ip_off;            /* fragment offset field */
+#define IP_RF 0x8000                /* reserved fragment flag */
+#define IP_DF 0x4000                /* dont fragment flag */
+#define IP_MF 0x2000                /* more fragments flag */
+#define IP_OFFMASK 0x1fff           /* mask for fragmenting bits */
+        uint8_t ip_ttl;             /* time to live */
+        uint8_t ip_p;               /* protocol */
+        uint16_t ip_sum;            /* checksum */
+        uint32_t ip_src, ip_dst;    /* source and dest address */
+} __attribute__ ((packed));
 typedef struct sr_ip_hdr sr_ip_hdr_t;
 
-/* 
+/*
  *  Ethernet packet header prototype.  Too many O/S's define this differently.
  *  Easy enough to solve that and define it here.
  */
@@ -142,46 +142,46 @@ struct sr_ethernet_hdr
 #ifndef ETHER_ADDR_LEN
 #define ETHER_ADDR_LEN 6
 #endif
-    uint8_t  ether_dhost[ETHER_ADDR_LEN];    /* destination ethernet address */
-    uint8_t  ether_shost[ETHER_ADDR_LEN];    /* source ethernet address */
-    uint16_t ether_type;                     /* packet type ID */
-} __attribute__ ((packed)) ;
+        uint8_t ether_dhost[ETHER_ADDR_LEN]; /* destination ethernet address */
+        uint8_t ether_shost[ETHER_ADDR_LEN]; /* source ethernet address */
+        uint16_t ether_type;                 /* packet type ID */
+} __attribute__ ((packed));
 typedef struct sr_ethernet_hdr sr_ethernet_hdr_t;
 
 
 
 enum sr_ip_protocol {
-  ip_protocol_icmp = 0x0001,
+        ip_protocol_icmp = 0x0001,
 };
 
 enum sr_ethertype {
-  ethertype_arp = 0x0806,
-  ethertype_ip = 0x0800,
+        ethertype_arp = 0x0806,
+        ethertype_ip = 0x0800,
 };
 
 
 enum sr_arp_opcode {
-  arp_op_request = 0x0001,
-  arp_op_reply = 0x0002,
+        arp_op_request = 0x0001,
+        arp_op_reply = 0x0002,
 };
 
 enum sr_arp_hrd_fmt {
-  arp_hrd_ethernet = 0x0001,
+        arp_hrd_ethernet = 0x0001,
 };
 
 
 struct sr_arp_hdr
 {
-    unsigned short  ar_hrd;             /* format of hardware address   */
-    unsigned short  ar_pro;             /* format of protocol address   */
-    unsigned char   ar_hln;             /* length of hardware address   */
-    unsigned char   ar_pln;             /* length of protocol address   */
-    unsigned short  ar_op;              /* ARP opcode (command)         */
-    unsigned char   ar_sha[ETHER_ADDR_LEN];   /* sender hardware address      */
-    uint32_t        ar_sip;             /* sender IP address            */
-    unsigned char   ar_tha[ETHER_ADDR_LEN];   /* target hardware address      */
-    uint32_t        ar_tip;             /* target IP address            */
-} __attribute__ ((packed)) ;
+        unsigned short ar_hrd;          /* format of hardware address   */
+        unsigned short ar_pro;          /* format of protocol address   */
+        unsigned char ar_hln;           /* length of hardware address   */
+        unsigned char ar_pln;           /* length of protocol address   */
+        unsigned short ar_op;           /* ARP opcode (command)         */
+        unsigned char ar_sha[ETHER_ADDR_LEN]; /* sender hardware address      */
+        uint32_t ar_sip;                /* sender IP address            */
+        unsigned char ar_tha[ETHER_ADDR_LEN]; /* target hardware address      */
+        uint32_t ar_tip;                /* target IP address            */
+} __attribute__ ((packed));
 typedef struct sr_arp_hdr sr_arp_hdr_t;
 
 #define sr_IFACE_NAMELEN 32
--- router/sr_router.c
+++ router/sr_router.c
@@ -76,7 +76,7 @@ void sr_handlepacket(struct sr_instance* sr,
         assert(packet);
         assert(interface);
 
-        printf("*** -> Received packet of length %d \n",len);
+        printf("\n*** -> Received packet of length %d \n",len);
 
         /* fill in code here */
         /*sr_ethernet_hdr_t* header = (sr_ethernet_hdr_t*) packet; */
@@ -129,28 +129,42 @@ int process_ARP(struct sr_instance* sr,
                 return -1;
         }
 
+        return 0; /* Temp: So make gcc doesn't lose it's shit */
+
+
 }
 
 /* Function to process the logic behind the IP Packet */
 int process_IP(struct sr_instance* sr,
-                uint8_t * ipPacket /* lent */,
-                unsigned int ipLength,
-                char* interface /* lent */)
+               uint8_t * ipPacket /* lent */,
+               unsigned int ipLength,
+               char* interface /* lent */)
 {
         printf("Entered IP packet processing block \n");
         /* Sanity Check */
         /* Check the correct length of a IP packet
          * ipLength = IP Header + IP Header
          * both values can be found in sr_protocol.h */
-        int minimum_ipLength = sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t);
+        int ethernetHeaderSize = sizeof(sr_ethernet_hdr_t);
+        int ipHeaderSize = sizeof(sr_ip_hdr_t);
+        int minimum_ipLength = ethernetHeaderSize + ipHeaderSize;
 
         if (ipLength < minimum_ipLength) {
-                fprintf(stderr, "invalid IP length");
+                fprintf(stderr, "invalid IP length!\n");
                 return -1;
         }
 
-        /* Deal with the IP packet
-           */
+        /* Construct the IP Header and deal with it */
+        sr_ip_hdr_t *ip_header = (sr_ip_hdr_t *)(packet + ethernetHeaderSize);
+
+        /* Sanity Check #2: Verify checksums via cksum() from sr_utils.c */
+        uint16_t cksum_result = cksum(ip_header, ip_header->ip_h1);
+        if (!cksum_result) {
+                fprintf(stderr, "Checksum failed!\n");
+                return -1;
+        }
 
 
+
+        return 0; /* Temp: So make gcc doesn't lose it's shit */
 }
